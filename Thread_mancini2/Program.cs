49dcc65c20aa0f7f4ed4fee020cbd4e9fa521d5f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-22 09:16:51 +0200

    modificato il programma, aggiungendo 3 thread e osservando il loro funzionamento, cioè come arrivano e chi ha la precedenza.

--- Thread_mancini2/Program.cs
+++ Thread_mancini2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Thread_mancini2
@@ -9,8 +10,20 @@ namespace Thread_mancini2
     class Program
     {
         static void Main(string[] args)
-
         {
+            Thread mythread = new Thread(() =>
+            {
+                  Console.WriteLine("My thread è iniziato");
+                  Thread.Sleep(1000);//tempo di attesa (1 secondo)
+                  Console.WriteLine("My thread è terminato");
+            });
+            //esecuzione di my thread
+            mythread.Start();
+
+            Thread.Sleep(500);// 1/2 secondo
+            Console.WriteLine("Main thread");
+
+            Console.ReadLine();
 
         }
     }
