e7d0c7028c633d69114a5315a270e8244dec370c
studenti <studenti@rosselli.loc>
2019-10-22 08:58:52 +0200

    Aggiungere i file di progetto.

---
+++ Thread_mancini2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread_mancini2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+        }
+    }
+}
